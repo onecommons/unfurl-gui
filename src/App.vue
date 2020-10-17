43e6046e3486c2e744d7cbe90ee793c414146ec2
Matthew Souzis <matthew@souzis.com>
2020-10-17 01:59:28 -0700

    gitlab-ui test first pass

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <formTest />
     <img alt="Vue logo" src="./assets/logo.png" />
     <ApolloExample msg="Welcome to Your Vue.js App" />
   </div>
@@ -7,16 +8,24 @@
 
 <script>
 import ApolloExample from "./components/ApolloExample.vue";
+import formTest from "./components/test.vue";
 
 export default {
   name: "App",
   components: {
-    ApolloExample
+    ApolloExample,
+    formTest
   }
 };
 </script>
 
 <style>
+/* 
+  https://gitlab.com/gitlab-org/gitlab-ui/-/blob/master/doc/css.md
+    NOTE: "simple import" actually needs tilde ~ before module path as per
+    https://stackoverflow.com/questions/55933835/cannot-import-stylesheets-from-node-modules-without-tilde 
+*/
+@import "~@gitlab/ui/dist/index.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { createProvider } from "./vue-apollo";
+import setConfigs from "@gitlab/ui/dist/config";
+setConfigs();
 
 Vue.config.productionTip = false;
 
