021a338e45960e141227bf825e4f31e677e340d8
Matthew Souzis <matthew@souzis.com>
2020-12-29 08:51:53 -0800

    added DataTable, fixed lint errs

---
+++ src/components/DataTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <gl-table :items="this.items" :fields="$options.fields">
+    <template #head(column_one)>
+      <div>{{ items }}</div>
+    </template>
+    <template #cell(column_one)>
+      This is the template for column data belonging to the first object
+    </template>
+  </gl-table>
+</template>
+<script>
+import * as GlComponents from "@gitlab/ui";
+
+export default {
+  fields: [
+    {
+      key: "row",
+      label: "Column One",
+      thClass: "",
+      tdClass: ""
+    },
+    {
+      key: "content",
+      label: "Column 2",
+      thClass: "",
+      tdClass: ""
+    }
+  ],
+  name: "DataTable",
+  props: {
+    items: Array
+  },
+  components: {
+    ...GlComponents
+  }
+};
+</script>
+<style></style>
