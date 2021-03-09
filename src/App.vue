999f06d8073a7a35faa7b998b60c58be579627c5
Vickoman <vicmandlagasca@gmail.com>
2021-03-09 12:12:40 -0500

    Fix Checbox Behavior

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
   <div id="app">
-  <pre>Views
-* User home
-  - Contexts: Connections, secrets, etc.
-  - Cloud accounts / organizations (or just instances?)
-  - Repos: (projects, deployment, plain/source code, ensembles?)
-* Notification/Inbox pane: Review upstream updates, Status changes, Other alerts
-* MyCloud view -- ensembles, instances, jobs, artifacts/source
-* Project view -- replaces readme in project repo? basically the same as user home plus ensembles
-* Plaform home: including the whole cloud
-* browse/search for available public ensembles / services to clone or connect to
-</pre>
-Ensemble View
-    <Demo name="demo" />
+    <Table />
+    <!-- <Demo name="demo" /> -->
     <!-- <ApolloExample msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
@@ -22,18 +11,20 @@ import { Component, Vue } from "vue-property-decorator";
 
 // import ApolloExample from "./components/ApolloExample.vue";
 import Demo from "./components/Demo.vue";
+import Table from "./components/Table/Table.vue";
 
 @Component({
   name: "App",
   components: {
-    Demo
+    Demo,
+    Table
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-/* 
+/*
   https://gitlab.com/gitlab-org/gitlab-ui/-/blob/master/doc/css.md
     NOTE: "simple import" actually needs tilde ~ before module path as per
     https://stackoverflow.com/questions/55933835/cannot-import-stylesheets-from-node-modules-without-tilde 
