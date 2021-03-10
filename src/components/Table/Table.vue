526eb26ed86607c96a41e9c803fa864955b4aaf8
Vickoman <vicmandlagasca@gmail.com>
2021-03-10 00:09:20 -0500

    Change the use of Apollo query

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -1,60 +1,50 @@
 <template>
-  <ApolloQuery :query="require('../../graphql/Accounts/Account.gql')">
-    <template slot-scope="{ result: { loading, error, data } }">
-      <!-- Loading -->
-      <div v-if="loading" class="loading apollo">Loading...</div>
+  <div v-if="loading" class="loading apollo">Loading...</div>
+  <div v-else-if="error" class="error apollo">An error occured</div>
+  <div v-else>
+    <p>
+      Selected Rows:<br />
+      {{ selectedRows }}
+    </p>
+    <gl-table
+      id="accounts-table"
+      :current-page="page"
+      :select-mode="selectMode"
+      @row-clicked="rowClicked"
+      :items="items"
+      :fields="fields"
+      :perPage="perPage"
+      ref="selectableTable"
+      primary-key="id"
+      :tbody-tr-class="tbodyRowClass"
+      show-empty
+    >
+      <!-- No records found -->
+      <template #empty="scope">
+        <div class="text-center my-2">{{ scope.emptyText }}</div>
+      </template>
 
-      <!-- Error -->
-      <div v-else-if="error" class="error apollo">An error occured</div>
-
-      <!-- Result -->
-      <div v-else-if="data" class="result apollo">
-        <p>
-          Selected Rows:<br />
-          {{ selected }}
-        </p>
-
-        <gl-table
-          id="accounts-table"
-          selectable
-          @row-selected="onRowSelected"
-          :current-page="page"
-          :select-mode="selectMode"
-          :items="data.accounts"
-          :fields="fields"
-          :perPage="perPage"
-          ref="selectableTable"
-          show-empty
-        >
-          <!-- No records found -->
-          <template #empty="scope">
-            <div class="text-center my-2">{{ scope.emptyText }}</div>
-          </template>
-          <!-- Checkoboxes -->
-          <template #cell(selected)="row">
-            <gl-form-checkbox
-              v-model="row.rowSelected"
-              @change="onRowClicked(row)"
-            ></gl-form-checkbox>
-          </template>
-        </gl-table>
-        <gl-pagination
-          v-model="page"
-          :per-page="perPage"
-          :total-items="data.accounts.length"
-          first-text="First"
-          prev-text="Prev"
-          next-text="Next"
-          last-text="Last"
-          aria-controls="accounts-table"
-        />
-      </div>
-    </template>
-  </ApolloQuery>
+      <!-- Checkbox to select row -->
+      <template v-slot:cell(selected)="{ item, field: { key } }">
+        <gl-form-checkbox v-model="item[key]"></gl-form-checkbox>
+      </template>
+    </gl-table>
+    <gl-pagination
+      v-model="page"
+      :per-page="perPage"
+      :total-items="items.length"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+      aria-controls="accounts-table"
+    />
+  </div>
 </template>
 <script lang="ts">
 import { GlTable, GlPagination, GlFormCheckbox } from "@gitlab/ui";
 import { Component, Vue, Ref } from "vue-property-decorator";
+import { GET_ACCOUNTS } from "../../graphql/Accounts/Account";
 
 @Component({
   components: {
@@ -64,6 +54,7 @@ import { Component, Vue, Ref } from "vue-property-decorator";
   }
 })
 export default class Table extends Vue {
+  [x: string]: any;
   @Ref() readonly selectableTable: GlTable;
   fields: Array<any> = [
     {
@@ -108,22 +99,52 @@ export default class Table extends Vue {
     }
   ];
   page = 1;
-  perPage = 10;
+  perPage = 5;
+  loading = false;
+  error = false;
   selected: Array<any> = [];
   selectMode = "multi";
 
-  // Save the items selected within array.
-  onRowSelected(items: any) {
-    this.selected = items;
+  data() {
+    return {
+      items: []
+    };
   }
 
-  // Handle Toggle function in Checkboxes
-  onRowClicked(row: any) {
-    if (row.rowSelected) {
-      this.$refs.selectableTable.$children[0].unselectRow(row.index);
+  rowClicked(item: any) {
+    if (item.selected) {
+      this.$set(item, "selected", false);
     } else {
-      this.$refs.selectableTable.$children[0].selectRow(row.index);
+      this.$set(item, "selected", true);
     }
+  }
+
+  tbodyRowClass(item: any) {
+    /* Style the row as needed */
+    if (item.selected) {
+      return ["b-table-row-selected", "table-primary", "cursor-pointer"];
+    } else {
+      return ["cursor-pointer"];
+    }
+  }
+
+  public async created() {
+    this.loading = true;
+
+    const { loading, data, errors } = await this.$apollo.query({
+      query: GET_ACCOUNTS
+    });
+
+    this.loading = loading;
+    if (errors) {
+      this.error = true;
+    } else {
+      this.items = data.accounts;
+    }
+  }
+
+  public get selectedRows(): string[] {
+    return this.items.filter((item: { selected: any }) => item.selected);
   }
 }
 </script>
