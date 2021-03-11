b7909d07a4e7108e50a34c7cfb2c2e7ab02c1795
Vickoman <vicmandlagasca@gmail.com>
2021-03-11 12:28:15 -0500

    Fix conflicts merge from main

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,20 @@ module.exports = {
     node: true
   },
 
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
+  ],
 
   parserOptions: {
-    parser: "babel-eslint"
+    ecmaVersion: 2020
   },
-
   rules: {
-    "no-console": "off",
-    "no-debugger": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "graphql/template-strings": [
       "error",
       {
