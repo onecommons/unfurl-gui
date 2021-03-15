68ae99857f4968d276180b028e7c42e0b4ef204d
Victor Diaz <victor@onecommons.org>
2021-03-15 16:28:53 -0500

    Create Router file

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "../views/HomeView.vue";
+import TableView from "../views/TableView.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: Home,
+      name: "Home",
+      meta: {
+        title: "Home"
+      }
+    },
+    {
+      path: "/table",
+      component: TableView,
+      name: "Table",
+      meta: {
+        title: "Home"
+      }
+    }
+  ]
+});
