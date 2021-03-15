3ae97041cf032adaac238472529a009dc3fcb997
Victor Diaz <victor@onecommons.org>
2021-03-15 16:29:05 -0500

    Reference router into main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import { createProvider } from "./vue-apollo";
 import setConfigs from "@gitlab/ui/dist/config";
+import router from "./router";
 setConfigs();
 
 Vue.config.productionTip = false;
 
 new Vue({
   apolloProvider: createProvider(),
+  router,
   render: h => h(App)
 }).$mount("#app");
