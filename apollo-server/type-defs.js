c95f032369c45fe771c4090d4ebe95d3d286b740
Adam Souzis <adam@souzis.com>
2021-12-25 11:08:54 -0800

    make graphql linter aware of client-side only schemas

--- apollo-server/type-defs.js
+++ apollo-server/type-defs.js
@@ -1,7 +1,13 @@
 import fs from 'fs'
 import path from 'path'
 
-export default [
-    fs.readFileSync(path.resolve(__dirname, './schema.graphql'), { encoding: 'utf8' }),
-    fs.readFileSync(path.resolve(__dirname, './gitlab_schema.graphql'), { encoding: 'utf8' })
-]
+const typeDefs = [
+  fs.readFileSync(path.resolve(__dirname, './schema.graphql'), { encoding: 'utf8' }),
+  fs.readFileSync(path.resolve(__dirname, './gitlab_schema.graphql'), { encoding: 'utf8' })
+];
+
+// we need to include the client schema when generating the schema json used by the `graphql/template-strings` eslinter plugin
+if (process.env.APOLLO_SCHEMA_GENERATE) 
+  typeDefs.push(fs.readFileSync(path.resolve(__dirname, '../src/gitlab-oc/vue_shared/graphql/client-schema.graphql'), { encoding: 'utf8' }))
+
+export default typeDefs;
