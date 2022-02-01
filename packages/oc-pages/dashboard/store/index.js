33b1727822e9ce20e44fc596747c041b153e96f6
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-02-01 04:59:22 +0000

    Use getDeployments to populate dashboard

---
+++ packages/oc-pages/dashboard/store/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import createLogger from "vuex/dist/logger";
+//import environments from "./modules/environments";
+//import project from "./modules/project";
+//import deployment_template_updates from "./modules/deployment_template_updates";
+//import templateResources from "./modules/template_resources";
+import deployments from './modules/deployments';
+import project_application_blueprint from '../../project_overview/store/modules/project_application_blueprint';
+//import misc from './modules/misc';
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== "production";
+
+const store = new Vuex.Store({
+    modules: {
+        project_application_blueprint,
+        deployments
+    },
+    strict: debug,
+    plugins: debug && process.env.VUEX_LOGGER === "true" ? [createLogger()] : [],
+});
+
+export default store;
---
+++ packages/oc-pages/dashboard/store/modules/project_application_blueprint.js
@@ -0,0 +1,1 @@
+../../../project_overview/store/modules/project_application_blueprint.js
