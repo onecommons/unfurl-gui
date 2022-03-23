054cab7f1afedcb505a116fe148157eafe931d14
Adam Souzis <adam@souzis.com>
2022-03-22 17:23:25 -0700

    stop lint spam

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "semi": "off",
     "graphql/template-strings": [
       "warn", //eslint hates me
       {
