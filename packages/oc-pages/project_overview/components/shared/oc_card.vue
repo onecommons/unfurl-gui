ec6ab009143aab0c64b3a40a88e70c49f5e0bfe5
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-04-20 05:35:22 +0200

    Overview header alignment, incomplete status tooltip, overview deployment count

--- packages/oc-pages/project_overview/components/shared/oc_card.vue
+++ packages/oc-pages/project_overview/components/shared/oc_card.vue
@@ -1,5 +1,5 @@
 <script>
-import { GlButton, GlCard, GlIcon, GlBadge} from "@gitlab/ui";
+import { GlButton, GlCard, GlIcon, GlBadge, GlTooltipDirective} from "@gitlab/ui";
 import commonMethods from '../mixins/commonMethods';
 import {mapGetters} from 'vuex'
 import { bus } from 'oc_vue_shared/bus';
@@ -11,6 +11,9 @@ import { __ } from '~/locale';
 
 export default {
     name: "OcCard",
+    directives: {
+        GlTooltip: GlTooltipDirective, 
+    },
     components: {
         GlButton,
         GlCard,
@@ -148,8 +151,10 @@ export default {
                             <gl-icon
                                 v-if="displayValidation"
                                 :size="14"
+                                v-gl-tooltip.hover
                                 :class="['gl-ml-3', cardIsValid(card)? 'icon-green': '']"
                                 :name="cardIsValid(card)? 'check-circle-filled': 'status_preparing'"
+                                :title="cardIsValid(card)? 'Complete': `${customTitle || card.title} is Incomplete`"
                                 />
                             <gl-badge v-if="!isMobileLayout && badgeHeaderText" size="sm" class="gl-tab-counter-badge gl-ml-3 badge-oc-card" >{{ badgeHeaderText }}</gl-badge >
                         </div>
