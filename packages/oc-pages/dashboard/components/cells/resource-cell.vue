0b304be8dbacf63e0def9b9512d217a9b16f14a5
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-04-20 06:17:39 +0200

    Table resource visibility

--- packages/oc-pages/dashboard/components/cells/resource-cell.vue
+++ packages/oc-pages/dashboard/components/cells/resource-cell.vue
@@ -21,6 +21,10 @@ export default {
             return generateCardId(this.resource.name)
         },
         to() {
+            let consoleUrl
+            if(consoleUrl = this.resource.attributes?.find(a => a.name == 'console_url')?.value) {
+                return {href: consoleUrl}
+            }
             const href = this.noRouter?
                 `/dashboard/deployments/${this.environment.name}/${this.deployment.name}#${this.id}`: // TODO use from routes.js
                 {name: routes.OC_DASHBOARD_DEPLOYMENTS, params: {name: this.deployment.name, environment: this.environment.name}, hash: `#${this.id}`}
