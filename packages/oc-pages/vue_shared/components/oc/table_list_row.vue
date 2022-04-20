f02f84ddb0c0964afd94512da99cf27e322a7e3f
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-04-20 14:04:19 +0200

    UI tweaks

--- packages/oc-pages/vue_shared/components/oc/table_list_row.vue
+++ packages/oc-pages/vue_shared/components/oc/table_list_row.vue
@@ -43,7 +43,7 @@ export default {
         <div v-if="items.length > 0">
             <div v-for="(item, index) in items.filter(item => item)" :key="item + index" class="gl-responsive-table-row oc_table_row">
                 <div class="table-section oc-table-section section-wrap text-truncate section-25">
-                    <span class="text-break-word title">{{ item.title || item.name }}</span>
+                    <span class=" title">{{ item.title || item.name }}</span>
                 </div>
                 <div class="table-section oc-table-section section-wrap text-truncate section-40">
                     <div class="light-gray">{{ item.description }}</div>
@@ -56,12 +56,12 @@ export default {
                                 name="upload"
                                 class="options-expanded-icon gl-mr-1"/>
                         </span>
-                        <span class="text-break-word ">{{ __(`${item.totalDeployments} Deployments`)  }}</span>
+                        <span class=" ">{{ __(`${item.totalDeployments} Deployments`)  }}</span>
                     </a>
                 </div>
 
                 <div class="table-section oc-table-section section-wrap text-truncate section-20">
-                    <span class="text-break-word deploy-button-group">
+                    <span class=" deploy-button-group">
                             <gl-button
                                 v-if="editable"
                                 :title="__('Edit')"
