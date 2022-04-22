76c548aabff1b5ab9aa5774ef210d98a4e614908
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-04-22 03:06:26 +0200

    Don't allow saving incomplete external resources https://github.com/onecommons/gitlab-oc/issues/467

--- packages/oc-pages/dashboard/dashboard.vue
+++ packages/oc-pages/dashboard/dashboard.vue
@@ -38,6 +38,12 @@ export default {
         this.newEnvironmentProvider = this.$route.query?.provider || sessionStorage['instantiate_provider']
         const expectsCloudProvider = sessionStorage['expect_cloud_provider_for']
 
+        const query = {...this.$route.query}
+        let deletedQuery = delete query.env
+        deletedQuery = delete query.provider || deletedQuery
+
+        if(deletedQuery) {this.$router.replace({query})}
+
         delete sessionStorage['instantiate_env']
         delete sessionStorage['instantiate_provider']
 
