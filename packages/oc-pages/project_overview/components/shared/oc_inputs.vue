4e23d4d66aa05475fe91ae42208687114204c88b
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-07-30 00:30:38 +0200

    Correctly serialize environments map

--- packages/oc-pages/project_overview/components/shared/oc_inputs.vue
+++ packages/oc-pages/project_overview/components/shared/oc_inputs.vue
@@ -161,11 +161,17 @@ export default {
                 properties: {
                   key: {
                     'x-decorator': 'FormItem',
-                    'x-component': 'Input'
+                    'x-component': 'Input',
+                    'x-component-props': {
+                      placeholder: 'key'
+                    }
                   },
                   value: {
                     'x-decorator': 'FormItem',
                     'x-component': ComponentMap[currentValue.additionalProperties.type],
+                    'x-component-props': {
+                      placeholder: 'value'
+                    }
                   },
                   remove: {
                     type: 'void',
@@ -253,9 +259,20 @@ export default {
         this.saveTriggers[propertyName] = triggerFn = _.debounce((function(field, value, disregardUncommitted=false) {
           // TODO move cloneDeep/serializer into another function
           const propertyValue = _.cloneDeepWith(value, function(value) {
-            if(Array.isArray(value) && value.length > 0) {
-              return value.map(o => o?.input? o?.input: o)
-            }
+            if(Array.isArray(value)) {
+              if (value.length == 0) {
+                return null
+              }
+              if (value.some(o => o?.input !== undefined)) {
+                return value.map(o => o?.input? o?.input: o)
+              } else if (value.some(o => o?.key !== undefined)) {
+                const result = {}
+                for(const entry of value)  {
+                  result[entry.key] = entry.value
+                }
+                return result
+              }
+            } 
           })
           this.updateProperty({deploymentName: this.$route.params.slug, templateName: this.card.name, propertyName: field.name, propertyValue, isSensitive: field.sensitive, nestedPropName: this.nestedProp?.name})
           if(disregardUncommitted && !this.card._uncommitted) this.clientDisregardUncommitted()
@@ -283,15 +300,19 @@ export default {
 
           const next = {...property, ...definition}
 
+          const isMap = next.additionalProperties && _.isObject(next.value)
+          if(isMap) { // I don't know how to handle this in clone deep
+            next.value = Object.entries(next.value).map(([key, value]) => ({key, value}))
+          }
+
           /*
            * uncomment to default to minimum
           if(next.type == 'number' && next.required && next.minimum && (next.default ?? null) === null) {
             next.default = next.minimum
           }
           */
-
           next.value = _.cloneDeepWith(next.value ?? next.default, function(value) {
-            if(Array.isArray(value) && value.length > 0) {
+            if(Array.isArray(value) && value.length > 0 && !isMap) {
               return value.map(input => ({input}))
             }
             if(value?.get_env) {
