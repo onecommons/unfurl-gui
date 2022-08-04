6c726dce74f2b24e08396cee69a8718131d662cf
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-08-04 09:24:54 +0200

    Refactor to be consistent with dispatch pipeline output

---
+++ packages/oc-pages/dashboard/components/tables/deployment-index-table/last-deploy.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'LastDeploy',
+    props: {
+        deploymentItem: Object
+    },
+    data() {
+        return {
+            createdAtText: null,
+        }
+    },
+    watch: {
+        deploymentItem: {
+            immediate: true,
+            async handler(item) {
+                this.createdAtText = await item.getCreatedAtText() 
+            }
+        }
+    }
+}
+
+</script>
+<template>
+    <div>
+        {{createdAtText}}
+        <div v-if="createdAtText">
+            <span v-if="deploymentItem.consoleLink">
+                <a :href="deploymentItem.consoleLink">View Job {{deploymentItem.jobStatusMessage}}</a> /
+                <a :href="deploymentItem.artifactsLink">View Artifacts</a>
+            </span>
+        </div>
+    </div>
+</template>
