6429d250546f8b38d5e1ffcf100bcff9b6c8ebca
Adam Souzis <adam@souzis.com>
2022-10-23 22:02:42 -0700

    Fix local command when the deployment ensemble hasn't been created yet

--- packages/oc-pages/vue_shared/client_utils/unfurl-invocations.js
+++ packages/oc-pages/vue_shared/client_utils/unfurl-invocations.js
@@ -1,27 +1,22 @@
-export function cloneAlreadyDeployed({
-    protocol, username, token, server, projectPath, projectId, environmentName, deployPath
-}) {
-  const vars_url = unfurl_cloud_vars_url({protocol, token, server, projectId});
-  return (
-`unfurl clone '${protocol}//${username}:${token}@${server}/${projectPath}#:${deployPath}' --var UNFURL_CLOUD_VARS_URL '${vars_url}' --use-environment ${environmentName}`
-    )
-}
-
-export function unfurl_cloud_vars_url({
+function unfurl_cloud_vars_url({
   protocol, token, server, projectId
 }) {
   return `${protocol}//${server}/api/v4/projects/${projectId}/variables?per_page=1000&private_token=\${UNFURL_ACCESS_TOKEN:${token}}`
 }
 
-/*
- * unfurl clone https://<username>:<token>@<unfurl.server>/<project_path> --existing --overwrite --var UNFURL_CLOUD_VARS_URL 'https://<unfurl.server>/api/v4/projects/{project_id}/variables?filter=${ENVIRONMENT:*}&private_token=<token>' --mono --use-environment <environment> --use-deployment-blueprint <deployment> --skeleton dashboard <BLUEPRINT_PROJECT_URL> <DEPLOY_PATH>
- */
-
-export function cloneForDraft({
-    protocol, username, token, server, projectPath, projectId, environmentName, deploymentName, deployPath, blueprintUrl, blueprint
+export function cloneProject({
+    protocol, username, token, server, projectPath, projectId
 }) {
-    const vars_url = unfurl_cloud_vars_url({protocol, token, server, projectId});
+  const vars_url = unfurl_cloud_vars_url({protocol, token, server, projectId});
+  return (
+    `unfurl clone '${protocol}//${username}:${token}@${server}/${projectPath}' --var UNFURL_CLOUD_VARS_URL '${vars_url}'`
+  )
+}
+
+export function cloneBlueprint({
+  projectName, environmentName, deploymentName, deployPath, blueprintUrl
+}) {
     return (
-`unfurl clone '${protocol}//${username}:${token}@${server}/${projectPath}' --existing --overwrite --var UNFURL_CLOUD_VARS_URL '${vars_url}' --mono --use-environment ${environmentName} --use-deployment-blueprint ${blueprint} --skeleton dashboard ${blueprintUrl} ${deployPath}`
+      `cd ${projectName}; unfurl clone --existing --overwrite --mono --use-environment ${environmentName} --use-deployment-blueprint ${deploymentName} --skeleton dashboard ${blueprintUrl} ${deployPath}`
     )
 }
--- packages/oc-pages/vue_shared/components/oc/local-deploy.vue
+++ packages/oc-pages/vue_shared/components/oc/local-deploy.vue
@@ -1,7 +1,7 @@
 <script>
 import CodeClipboard from './code-clipboard.vue'
 import {mapGetters} from 'vuex'
-import {cloneAlreadyDeployed, cloneForDraft} from 'oc_vue_shared/client_utils/unfurl-invocations'
+import {cloneProject, cloneBlueprint} from 'oc_vue_shared/client_utils/unfurl-invocations'
 
 
 export default {
@@ -20,7 +20,7 @@ export default {
             'lookupVariableByEnvironment',
             'lookupDeployPath'
         ]),
-        localCloneInvocation() {
+        _localCloneOptions() {
             const protocol = window.location.protocol
             const username = this.getUsername
             const token = this.lookupVariableByEnvironment('UNFURL_ACCESS_TOKEN', '*')
@@ -30,28 +30,28 @@ export default {
             }
             const projectPath = this.getHomeProjectPath
             const projectId = window.gon.projectId // TODO fix gon.projectId hack
+            const projectName = this.getHomeProjectPath.split('/').pop()            
             const deploymentName = this.deployment.name
             const environmentName = this.environment.name
             const deployPath = this.lookupDeployPath(deploymentName, environmentName)?.name
             const blueprint = this.deployment.blueprint
             const blueprintUrl = `${protocol}//${username}:${token}@${server}/${this.deployment.projectPath}`
-
-            const options = {protocol, username, token, server, projectPath, projectId, environmentName, deploymentName, deployPath, blueprint, blueprintUrl}
-            if(this.deployment.__typename == 'DeploymentTemplate') {
-                return cloneForDraft(options)
-            } else {
-                return cloneAlreadyDeployed(options)
-            }
+            return { protocol, username, token, server, projectName, projectPath, projectId, environmentName, deploymentName, deployPath, blueprint, blueprintUrl }
+        },
+        localCreateDeploymentInvocation() {
+            return cloneBlueprint(this._localCloneOptions())
+        },
+        localCloneInvocation() {
+            return cloneProject(this._localCloneOptions())
         },
         localDeployInvocation() {
             const projectName = this.getHomeProjectPath.split('/').pop()
             const deploymentName = this.deployment.name
             const environmentName = this.environment.name
             const deployPath = this.lookupDeployPath(deploymentName, environmentName)?.name
-            const protocol = window.location.protocol
-            const token = this.lookupVariableByEnvironment('UNFURL_ACCESS_TOKEN', '*')
             return `cd ${projectName}; unfurl deploy ${deployPath} --use-environment ${environmentName} --commit`
-        }
+        },
+        deploymentExists() { return deployment.__typename != 'DeploymentTemplate' }
     }
 }
 
@@ -60,15 +60,19 @@ export default {
     <div>
         <p>Run these shell commands to deploy this on your local machine:</p>
         <p>
-            Install Unfurl:
+            Install Unfurl if needed:
             <code-clipboard class="mt-1">python -m pip install -U unfurl</code-clipboard>
         </p>
-        <p>
-            Clone from Unfurl.cloud:
+        <p >
+            Clone this Unfurl project if you haven't already:
             <code-clipboard class="mt-1">{{localCloneInvocation}}</code-clipboard>
         </p>
+        <p v-if="!deploymentExists"> 
+            Create the deployment from the blueprint.
+            <code-clipboard class="mt-1">{{localCreateDeploymentInvocation}}</code-clipboard>
+        </p>
         <p>
-            Deploy your application:
+            Deploy the blueprint:
             <code-clipboard class="mt-1">{{localDeployInvocation}}</code-clipboard>
         </p>
         <p>Learn more at <a href="https://github.com/onecommons/unfurl" target="_blank">https://github.com/onecommons/unfurl</a>.</p>
