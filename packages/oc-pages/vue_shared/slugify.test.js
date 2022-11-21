d2beddcf810448d48cf6ad7c303246f56b26a1ff
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-11-21 18:02:08 +0100

    Follow unfurl slugify behavior of collapsing '-' characters after replacing whitespace (#265)

--- packages/oc-pages/vue_shared/slugify.test.js
+++ packages/oc-pages/vue_shared/slugify.test.js
@@ -1,13 +1,30 @@
 import slugify from './slugify'
+
+function expectMatchingSlug(text) {
+    expect(slugify(text)).toMatch(/^[A-Za-z._][A-Za-z0-9._\\-]*$/)
+}
 function testSlugifyInvocation(text) {
     test(`slugify ${text}`, () => {
-        expect(slugify(text)).toMatch(/^[A-Za-z._][A-Za-z0-9._\\-]*$/)
+        expectMatchingSlug(text)
     })
 
 }
 
 testSlugifyInvocation('422a')
-test('slugify 422', () => {
-  expect(slugify('422')).toEqual('')
-})
 testSlugifyInvocation('a422')
+
+test('slugify 422', () => {
+    expect(slugify('422')).toEqual('')
+})
+
+test('unfurl slugify behavior (title)', () => {
+    const s = 'Google Cloud Platform - Compute Engine 2'
+    expectMatchingSlug(s)
+    expect(slugify(s)).toEqual('google-cloud-platform-compute-engine-2')
+})
+
+test('unfurl slugify behavior (name)', () => {
+    const s = 'google-cloud-platform---compute-engine-2'
+    expectMatchingSlug(s)
+    expect(slugify(s)).toEqual('google-cloud-platform-compute-engine-2')
+})
