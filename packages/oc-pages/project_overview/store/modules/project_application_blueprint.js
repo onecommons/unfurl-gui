d282ca24d55f3ef08be3f7a6c17c8e3eae3af3c6
Andrew Breidenbach <breidenbach.aj@gmail.com>
2022-11-27 02:46:35 +0100

    Adtl providers: support removal and property updates

--- packages/oc-pages/project_overview/store/modules/project_application_blueprint.js
+++ packages/oc-pages/project_overview/store/modules/project_application_blueprint.js
@@ -190,7 +190,10 @@ const actions = {
                 for(const connection of de.connections) {
                     const providerType = connection?.type
                     if(!root.ResourceTemplate) { root.ResourceTemplate = {} }
-                    root.ResourceTemplate[connection.name] = _.cloneDeep(connection)
+
+                    // intentionally not cloning
+                    // we want to normalize this record in both places
+                    root.ResourceTemplate[connection.name] = connection
                 }
             },
             repositories(entry) {
@@ -211,10 +214,21 @@ const actions = {
             if(!value || typeof value != 'object') continue
             if(!Object.isFrozen(value) && typeof transforms[key] == 'function')
                 Object.values(value).forEach(entry => {if(typeof entry == 'object' && !Object.isFrozen(entry)) {transforms[key](entry, root)}})
-            commit('setProjectState', {key, value})
+
+            // commit so we can use our resolvers while normalizing
+            if(key == 'ResourceType') {
+                commit('setProjectState', {key, value})
+            }
         }
         if(root.ApplicationBlueprint) {
             commit('setProjectState', {key: 'applicationBlueprint', value: Object.values(root.ApplicationBlueprint)[0]})
+        }
+
+        // second iteration to avoid mutating committed
+        for(const key of ordering) {
+            if(key == 'ResourceType') continue
+            const value = root[key]
+            commit('setProjectState', {key, value})
         }
     }
     
