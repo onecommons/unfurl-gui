7a6623d247ace6e20bf73cddf2ce5efd33a141c3
Andrew Breidenbach <breidenbach.aj@gmail.com>
2023-03-16 23:48:50 +0100

    Use fake password for formily
    This currently does not prevent the user from copying the mock password
    element's value

---
+++ packages/oc-pages/project_overview/components/shared/oc_inputs/fake-password.vue
@@ -0,0 +1,50 @@
+<script>
+import {Input as ElInput} from 'element-ui'
+
+export default {
+    name: 'FakePassword',
+    components: {
+        ElInput
+    },
+    props: {
+        value: String
+    },
+    data() {
+        return {
+            hidingPassword: true,
+        }
+    },
+    methods: {
+        input() {
+            this.$emit('input', ...arguments)
+        }
+    }
+}
+</script>
+<template>
+    <el-input
+        ref="input-el"
+        :value="value"
+        @input="input"
+        :class="{hidingPassword}"
+        type="text"
+    >
+        <template #suffix>
+            <i @click="hidingPassword = !hidingPassword" class="el-input__icon el-icon-view el-input__clear" />
+        </template>
+    </el-input>
+</template>
+<style>
+    @font-face {
+        font-family: 'Password';
+        font-style: normal;
+        font-weight: 400;
+        src: url(/oc/assets/fonts/Bullets.woff2) format('woff2');
+    }
+
+    .hidingPassword input {
+        letter-spacing: 0.5px;
+        padding-top: 4px;
+        font-family: Password;
+    }
+</style>
