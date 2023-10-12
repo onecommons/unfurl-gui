f0d7ebb0809c3abe03e2d420646185ee06d79621
Andrew Breidenbach <breidenbach.aj@gmail.com>
2023-10-12 23:23:19 +0200

    Cypress: attempt 2 at supporting package rules

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -120,7 +120,7 @@ beforeEach(() => {
       win.sessionStorage['unfurl-validation-mode'] = UNFURL_VALIDATION_MODE
     }
     if(UNFURL_SERVER_URL) {
-      cy.log(UNFURL_SERVER_URL)
+      cy.task('log', UNFURL_SERVER_URL)
       cy.intercept('/services/unfurl-server/*', (req) => {
         req.url = req.url.replace(/.*services\/unfurl-server/, UNFURL_SERVER_URL)
       })
@@ -128,7 +128,7 @@ beforeEach(() => {
       // win.sessionStorage['unfurl_gui:unfurl-server-url'] = UNFURL_SERVER_URL
     }
     if(UNFURL_PACKAGE_RULES) {
-      cy.log({UNFURL_PACKAGE_RULES})
+      cy.task('log', {UNFURL_PACKAGE_RULES})
       cy.intercept('/*/-/deployments/new', (req) => {
         req.data.pipeline.variables_attributes.push({
           key: 'UNFURL_PACKAGE_RULES',
