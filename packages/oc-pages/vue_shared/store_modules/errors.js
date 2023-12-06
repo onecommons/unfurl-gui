dabfe8d27b0c0772a54a7d30d054dc7e5eacda8e
Andrew Breidenbach <breidenbach.aj@gmail.com>
2023-12-06 01:22:42 +0100

    Show error tracebacks when detected in response details

--- packages/oc-pages/vue_shared/store_modules/errors.js
+++ packages/oc-pages/vue_shared/store_modules/errors.js
@@ -13,14 +13,19 @@ const state = stateFn()
 const mutations = {
     createError(state, {severity, message, context, ...optional}) {
         const now = new Date(Date.now())
-        
-        state.errors.push({
+
+        const traceback = context.details?.startsWith('Traceback')? context.details: undefined
+        const err = {
             severity: severity || 'major',
             message,
             context: context?.constructor?.name == 'Error'? context.message: _.cloneDeep(context),
             ...optional,
             time: now.toISOString()
-        })
+        }
+        if(traceback) {
+            err.traceback = traceback
+        }
+        state.errors.push(err)
     },
     clearErrors(state) {
         Vue.set(state, 'errorsClearedTo', state.errors.length)
