dac63b8b9ea7f6bc7f894742b2d4578b35aaba53
Andrew Breidenbach <breidenbach.aj@gmail.com>
2024-02-09 17:42:39 +0100

    Mark floatingip fixture as incompatible with v1 tests

--- cypress/e2e/blueprints/aws__nextcloud__floatingip.cy.js
+++ cypress/e2e/blueprints/aws__nextcloud__floatingip.cy.js
@@ -1,6 +1,12 @@
 import {deploymentFixturePath} from '../../support/deployment-fixture'
-describe('aws__nextcloud__floatingip', () => {
-  it('Can recreate deployment', () => {
-    cy.recreateDeployment(deploymentFixturePath('aws__nextcloud__floatingip'))
+
+const TEST_VERSIONS = Cypress.env('TEST_VERSIONS')
+
+// There's no v1 fixture for this test
+if(TEST_VERSIONS != 'v1') {
+  describe('aws__nextcloud__floatingip', () => {
+    it('Can recreate deployment', () => {
+      cy.recreateDeployment(deploymentFixturePath('aws__nextcloud__floatingip'))
+    })
   })
-})
+}
