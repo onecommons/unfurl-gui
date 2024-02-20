69a093512978de4ff6850bfd626f0a0cabebf7cb
Andrew Breidenbach <breidenbach.aj@gmail.com>
2024-02-20 20:54:47 +0100

    Cypress: Add option to skip nestedcloud

--- cypress/e2e/blueprints/az__nestedcloud__nestedcloud.cy.js
+++ cypress/e2e/blueprints/az__nestedcloud__nestedcloud.cy.js
@@ -1,6 +1,9 @@
 import {deploymentFixturePath} from '../../support/deployment-fixture'
+const SKIP_NESTEDCLOUD = Cypress.env("SKIP_NESTEDCLOUD")
 describe('az__nestedcloud__nestedcloud', () => {
-  it('Can recreate deployment', () => {
-    cy.recreateDeployment(deploymentFixturePath('az__nestedcloud__nestedcloud'))
-  })
+  if(!SKIP_NESTEDCLOUD) {
+    it('Can recreate deployment', () => {
+      cy.recreateDeployment(deploymentFixturePath('az__nestedcloud__nestedcloud'))
+    })
+  }
 })
--- cypress/e2e/blueprints/gcp__nestedcloud__nestedcloud.cy.js
+++ cypress/e2e/blueprints/gcp__nestedcloud__nestedcloud.cy.js
@@ -1,9 +1,10 @@
 import {deploymentFixturePath} from '../../support/deployment-fixture'
 
 const TEST_VERSIONS = Cypress.env('TEST_VERSIONS')
+const SKIP_NESTEDCLOUD = Cypress.env("SKIP_NESTEDCLOUD")
 
 // TODO test fixture known to not work on v1
-if(TEST_VERSIONS != 'v1') {
+if(TEST_VERSIONS != 'v1' && !SKIP_NESTEDCLOUD) {
   describe('gcp__nestedcloud__nestedcloud', () => {
     it('Can recreate deployment', () => {
       cy.recreateDeployment(deploymentFixturePath('gcp__nestedcloud__nestedcloud'))
