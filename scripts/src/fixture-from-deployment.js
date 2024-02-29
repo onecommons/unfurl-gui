fa607f84c75fcc61c80141486af91d1f211ee167
Andrew Breidenbach <breidenbach.aj@gmail.com>
2024-02-29 18:41:42 +0100

    Cypress: make fixture generation script nicer, redo v2 nestedcluod fixtures

--- scripts/src/fixture-from-deployment.js
+++ scripts/src/fixture-from-deployment.js
@@ -1,67 +1,76 @@
 #!/usr/bin/env node
 
 const TEST_VERSIONS = process.env.TEST_VERSIONS || 'v2'
+const axios = require('./shared/axios-instance.js')
+const login = require('./shared/login')
 const path = require('path')
 const {unfurlGuiRoot} = require('./shared/util.js')
 const fs = require('fs')
 const args = require('minimist')(process.argv.slice(2))
 
 const deploymentName = args.deployment || args['deployment-name']
-const generateTest = args['test-name'] || args['test'] || args['generate-test']
+const username = args.username
+const dashboard = args.dashboard || (args.username && `${args.username}/dashboard`)
+const unfurlServer = args.unfurlServer || args['unfurl-server'] || `${process.env.OC_URL}/services/unfurl-server`
+// const generateTest = args['test-name'] || args['test'] || args['generate-test']
+const fixtureName = args.fixtureName || args['fixture-name'] || args['test']
+const fixturePath = `generated/deployments/${TEST_VERSIONS}/${fixtureName}`
+const renameDeployment = args.rename
 
-const unfurlExport = JSON.parse(fs.readFileSync( 0, 'utf-8' ))
 
-function main() {
-  const deployPath = Object.keys(unfurlExport.DeploymentPath).find(path => path.endsWith(`/${deploymentName}`))
-  const pathComponents = deployPath.split('/')
-  pathComponents.pop()
-  const
-    blueprintName = pathComponents.pop()
-
-  let environmentName
-  while(pathComponents.length && pathComponents[pathComponents.length - 1] != 'environments') {
-    environmentName = pathComponents.pop()
-  }
-  pathComponents.pop()
-
-  const fixtureName = generateTest || `_${environmentName}__${blueprintName}__${deploymentName}`,
-    fixturePath = `generated/deployments/${TEST_VERSIONS}/${fixtureName}`
-
-  const DeploymentPath = { [deployPath]: unfurlExport.DeploymentPath[deployPath]}
-  const deployment = {
-    ...unfurlExport.deployments.find(dep => Object.values(dep.DeploymentTemplate || [[]])[0].name == deploymentName),
-    DeploymentPath
+async function main() {
+  const requiredParams = {
+    dashboard, deploymentName, fixtureName
   }
 
-  switch(Object.keys(DeploymentPath).length) {
-    case 0:
-      throw new Error('No deployment records found in environments.json')
-    case 1:
-      break
-    default:
-      throw new Error('Conflicting deployment records found in environments.json')
+  Object.entries(requiredParams).forEach(([key, value]) => {
+    if(!value) {
+      throw new Error(`Expected value for ${key}`)
+    }
+  })
+
+  await login(process.env.OC_URL, process.env.OC_USERNAME, process.env.OC_PASSWORD)
+  const response = await axios.get(
+    `${unfurlServer}/export?format=environments&branch=main&auth_project=${encodeURIComponent(dashboard)}&include_all_deployments=1`,
+    {
+      headers: {'x-git-credentials': btoa(`${process.env.OC_USERNAME}:${process.env.OC_PASSWORD}`)}
+    }
+  )
+
+  const deployment = response.data.deployments.find(deploymentDict => Object.values(deploymentDict.Deployment)[0].name == deploymentName)
+  const deployPath = Object.keys(response.data.DeploymentPath).find(path => path.endsWith(`/${deploymentName}`))
+
+  if(!(deployment && deployPath)) {
+    throw new Error('Deployment not found')
+  }
+
+  if(renameDeployment) {
+    Object.values(deployment.Deployment)[0].title = renameDeployment
+    Object.values(deployment.DeploymentTemplate)[0].title = renameDeployment
+  }
+
+  const fixture = {
+    ...deployment,
+    DeploymentPath: {
+      [deployPath]: response.data.DeploymentPath[deployPath]
+    }
   }
 
   fs.writeFileSync(
     path.join(unfurlGuiRoot, 'cypress/fixtures', `${fixturePath}.json`),
-    JSON.stringify(deployment)
+    JSON.stringify(fixture)
   )
 
-  if(generateTest) {
-    const testTemplate = fs.readFileSync(
-      path.join(__dirname, 'fixture-from-deployment', 'recreate-deployment.template.js'),
-      'utf-8'
-    )
-      .replace('$TEST_NAME', generateTest)
+  console.log(`${fixturePath}.json written`)
+}
 
-    fs.writeFileSync(
-      path.join(unfurlGuiRoot, 'cypress/e2e/blueprints', `${generateTest}.cy.js`),
-      testTemplate
-    )
+async function tryMain() {
+  try {
+    await main()
+  } catch(e) {
+    console.error(e.message)
   }
 }
 
-main()
 
-
-
+tryMain()
